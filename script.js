1f0ed25b3c82ad4ad135dc914544c3d111a95060
MonuSh-Git <monush.inbox@gmail.com>
2024-10-10 17:18:30 +0530

    Update script.js

--- script.js
+++ script.js
@@ -97,7 +97,7 @@ function startFrizaMovement() {
   setInterval(() => {
     frizaY = Math.random() * (gameContainer.offsetHeight - 100);
     friza.style.top = frizaY + "px";
-  }, 2000);
+  }, 1000);
 }
 
 startFrizaMovement();
@@ -136,7 +136,7 @@ function resetGame() {
   randomKiBlastAttack();
 }
 
-let kiBlastSpeed = 5;
+let kiBlastSpeed = 10;
 let kiBlastReleased = false;
 
 function releaseKiBlast() {
